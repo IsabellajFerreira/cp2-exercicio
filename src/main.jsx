dd7fce8d700543d34367dbbb0a85bf96b61f1a07
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-09-05 11:40:05 -0300

    Importando o navigate do dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import {createBrowserRouter, Routerprovider } from 'react-router-dom'
+import {createBrowserRouter, Routerprovider, Navigate } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
@@ -26,6 +26,10 @@ const router = createBrowserRouter([
       { 
         path: "/produtos/editar/:id", 
         element: <EditarProduto /> 
+      },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
       }
     ]
   }
