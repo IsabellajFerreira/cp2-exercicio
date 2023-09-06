580c808314dd3ecfd6cf35451567a686c74bb2a6
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-09-05 21:25:54 -0300

    Arrumando o menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function Menu(){
     return(
         <nav className='menu'>
-            <Link to='/'></Link>
+            <Link to='/'>Home</Link>
                 <span> | </span>
                 <Link to='/produtos'>Produtos</Link>
         </nav>
